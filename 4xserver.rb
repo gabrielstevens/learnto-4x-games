3e07c77b41e6d52f9027ce8b6bfed233ab153733
Gabriel Stevens <geekay_es@hotmail.com>
2014-12-05 16:05:42 -0600

    Map progress.
    Played around with the map class.

--- 4xserver.rb
+++ 4xserver.rb
@@ -1,20 +1,67 @@
 require './classes/unit.rb'
 require './classes/map.rb'
 
-Map.new(10, 10)
+world = Map.new(10, 10)
 
 # Create an array to hold the list of units.
 unitlist = []
 
+# Create a variable to hold the current turn.
+turn = 1
+
 # Create an initial unit and add it to the array.
-unitlist.push(Unit.new(1, "Bob", "settler", {'x' => 1, 'y' => 1}))
+unitlist.push(Unit.new(turn, "Bob", "settler", {'x' => 1, 'y' => 1}))
 
 # Create a variable to hold the currently selected unit. Set its initial state to
 # the first slot in the new unitlist array.
 selected = unitlist[0]
 
 # Call a unit from the array with the keyword "selected"
-selected.jrnl_write(1, "I can't wait to found a great civilization!")
+selected.jrnl_write(turn, "I can't wait to found a great civilization!")
 
 # This line calls a list of each unit in the array.
 #unitlist.each_with_index {|unit, i| puts "#{i}. #{unit.name} (#{unit.unit_type}) is at X:#{unit.location['x']},Y:#{unit.location['y']}"}
+
+def move(turn, selected, world, direction)
+  command = "On turn #{turn} I was commanded to lead my unit #{direction}. "
+  case direction
+  when "north"
+    if selected.location['y'] == world.max_ns
+      acknowledgement = "We were unable to go any farther north. "
+    else
+      selected.location['y'] += 1
+      acknowledgement = "We moved north one tile. "
+    end
+  when "south"
+    if selected.location['y'] == 1
+      acknowledgement = "We were unable to go any farther south. "
+    else
+      selected.location['y'] -= 1
+      acknowledgement = "We moved south one tile. "
+    end
+  when "east"
+    if selected.location['x'] == world.max_ns
+      selected.location['x'] = 1
+    else
+      selected.location['x'] += 1
+    end
+    acknowledgement = "We moved east one tile. "
+  when "west"
+    if selected.location['x'] == 1
+      selected.location['x'] = world.max_ns
+    else
+      selected.location['x'] -= 1
+    end
+    acknowledgement = "We moved west one tile. "
+  else
+  end
+  report = "Our location is X#{selected.location['x']}, Y#{selected.location['y']}."
+  selected.jrnl_write(turn, command + acknowledgement + report)
+end
+
+move(turn, selected, world, "south")
+#jrnl = selected.jrnl_read("all")
+#puts jrnl['title']
+#puts jrnl['content']
+
+world.tile_neighbors(4,4)
--- classes/map.rb
+++ classes/map.rb
@@ -3,19 +3,35 @@
 # for managing the viewable tiles of the board from the perspective of a unit
 # occupying that tile.
 class Map
-  attr_reader :max_ns      # size_north-south
-  attr_reader :max_ew      # size_east-west
+  attr_reader :max_ns   # maximum north-south size
+  attr_reader :max_ew   # maximum east-west size
+  attr_reader :tile     # the array of tiles
 
   def initialize(tall, wide)
     @max_ns = tall
     @max_ew = wide
 
+    # Create an array of tiles with X/Y coordinates and a tile base type.
+    @tile = []
     (1..tall).each do |y|
-      print "Row #{y}: Col 1"
-      (2..wide).each do |x|
-        print ".#{x}"
+      (1..wide).each do |x|
+        @tile.push({'x' => x, 'y' => y, 'base' => "ocean"})
       end
-      print "\n"
     end
+
+    # Pick a spot to start a continent.
+    rand_y = rand(3..tall - 2)
+    rand_x = rand(3..wide - 2)
+    continental_base = @tile.index({'x' => rand_x, 'y' => rand_y, 'base' => "ocean"})
+    @tile[continental_base]['base'] = "land"
+
+    # Now spiral around that spot, creating a larger land mass
+    cur_tile = @tile.index()
+  end
+
+  def tile_neighbors(x, y)
+    # First get the index of the tile with the given X & Y coordinates.
+    cur_tile = @tile.index({'x' => x, 'y' => y, 'base' => /\A(...)\z/})
+    puts "Current Tile: #{cur_tile}"
   end
 end
